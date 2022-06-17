1fe0db104354f486c07135bffa535ef8cd589f1d
Will Kahn-Greene <willkg@mozilla.com>
2022-06-17 16:45:22 -0400

    Fix typing errors

--- src/francis/testing.py
+++ src/francis/testing.py
@@ -1,31 +1,39 @@
 from contextlib import contextmanager
+from typing import Any, Callable, Dict, Generator, List
 
 import sentry_sdk
 from sentry_sdk.transport import Transport
 
 
 class CaptureTransport(Transport):
-    def __init__(self, capture_event_callback):
+    def __init__(self, capture_event_callback: Callable) -> None:
         Transport.__init__(self)
-        self.capture_event = capture_event_callback
-        self.capture_envelope = lambda *args, **kwargs: None
+        self.capture_event: Callable = capture_event_callback
+        self.capture_envelope: Callable = lambda *args, **kwargs: None
         self._queue = None
 
 
 class SentryTestHelper:
-    def __init__(self):
-        self.events = []
+    """Sentry test helper for initializing Sentry and capturing events.
 
-    def capture_event(self, event):
+    This lets you access the events emitted via the ``.events`` attribute.
+
+    """
+
+    def __init__(self) -> None:
+        self.events: List[Dict[Any, Any]] = []
+
+    def capture_event(self, event: Dict[Any, Any]) -> None:
         self.events.append(event)
 
     @contextmanager
-    def session_context(self):
+    def session_context(self) -> Generator["SentryTestHelper", None, None]:
         self.events = []
         with sentry_sdk.Hub(None):
             yield self
 
-    def init(self, *args, **kwargs):
+    def init(self, *args: Any, **kwargs: Any) -> None:
+        """Use the args for sentry_sdk.Client"""
         self.events = []
         hub = sentry_sdk.Hub.current
         client = sentry_sdk.Client(*args, **kwargs)
