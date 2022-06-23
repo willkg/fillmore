8bfc7dd4a6dc1a71f8203603fd00c5a0088e97b6
Will Kahn-Greene <willkg@mozilla.com>
2022-06-23 13:36:15 -0400

    Add license headers for all files

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from francis.test import SentryTestHelper
-
-
-@pytest.fixture
-def sentry_helper(request):
-    """Returns a Sentry test helper"""
-    return SentryTestHelper()
